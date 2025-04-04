0050e9182501bab32b71c4667f927d429d42f933
moises-henrique18 <moiseshenrique.1810@aluno.barueri.br>
2025-04-04 08:25:53 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,37 @@ import { useState } from "react"
 
 function App(){
 
-    const [nome, setNome] = useState('Ronaldo')
+    const [nome, setNome] = useState('Roberio')
 
-  return (
-    <div>
-      <h3>Pizzaria 2D</h3>
+    const Formulario = () => {
 
-      <input 
+      return (
+        <>
+          <h3>Formulário</h3>
+
+          <input 
        className="nome"
        onChange={(e)=>{setNome(e.target.value)}}
        type="text" />
     
     <button
       className="botao"
-      onClick={()=>{alert(nome)}}
+      onClick={(e)=>{alert('O nome digitado foi ' + nome)}}
     >
       CLIQUE AQUI
     </button>
+
+
+        </>
+      )
+
+    }
+
+  return (
+    <div>
+      <h3>Pizzaria 2D</h3>
+
+      <Formulario />  
 
     </div>
   )
